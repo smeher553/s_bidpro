985704f1062e9073df3116ff483f1a75fd06b990
sv3337 <svgdp3@gmail.com>
2026-01-20 22:39:38 +0530

    first commit

---
+++ s_bidpro/src/main/java/com/bid/bidpro/BidproApplication.java
@@ -0,0 +1,13 @@
+package com.bid.bidpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidproApplication.class, args);
+	}
+
+}
---
+++ s_bidpro/src/test/java/com/bid/bidpro/BidproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bid.bidpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
